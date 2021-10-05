92f709c8ba8ad1378bac97c8fb22cacf5bffe20c
Monthon Mukkun <79711492+carrynong2@users.noreply.github.com>
2021-10-05 11:20:33 +0700

    Add files via upload

---
+++ grading.py
@@ -0,0 +1,14 @@
+s = float(input())
+if s >= 80:
+  print("A")
+else:
+  if s >= 70:
+    print("B")
+  else:
+    if s >= 60:
+      print("C")
+    else:
+      if s >= 50:
+        print("D")
+      else:
+        print("F")
